48f1d1dda32793c54a74947e02606b830e36a0a4
jean philippe delabre <jeanphilippe.delabre@gmail.com>
2014-06-26 12:20:26 +0200

    Test

--- planning/src/main/java/com/github/aiderpmsi/planning/Jour.java
+++ planning/src/main/java/com/github/aiderpmsi/planning/Jour.java
@@ -4,6 +4,6 @@ import java.time.LocalDate;
 import solver.Solver;
 
 public interface Jour {
-	public void generatePlages(LocalDate date, Solver solver);
+	public void generatePlages(LocalDate date, Solver solver); 
 	public void generateConstraints(LocalDate date, Solver solver);
 }
